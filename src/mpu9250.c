8430cf1f05fd62301067ef45ff62025fc0da4ae1
OleksiiTeplyi <emblink182@gmail.com>
2021-10-10 13:37:33 +0300

    mpu hal changes

--- src/mpu9250.c
+++ src/mpu9250.c
@@ -19,11 +19,46 @@
 #define ZG_OFFSET_L 0x18
 #define SMPLRT_DIV 0x19
 #define CONFIG 0x1A
-#define ACCEL_CONFIG 0x1B
-#define ACCEL_CONFIG_2 0x1C
+#define GYRO_CONFIG  0x1B
+#define ACCEL_CONFIG 0x1C
+#define ACCEL_CONFIG_2 0x1D
 #define LP_ACCEL_ODR 0x1E
 #define WOM_THR 0x1F
 #define FIFO_EN 0x23
+/*** External i2c slave registers ***/
+#define INT_PIN_CFG 0x37
+#define INT_ENABLE 0x38
+#define INT_STATUS 0x3A
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_XOUT_L 0x3C
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_YOUT_L 0x3E
+#define ACCEL_ZOUT_H 0x3F
+#define ACCEL_ZOUT_L 0x40
+#define TEMP_OUT_H 0x41
+#define TEMP_OUT_L 0x42
+#define GYRO_XOUT_H 0x43
+#define GYRO_XOUT_L 0x44
+#define GYRO_YOUT_H 0x45
+#define GYRO_YOUT_L 0x46
+#define GYRO_ZOUT_H 0x47
+#define GYRO_ZOUT_L 0x48
+/*** External sensors data registers ***/
+#define SIGNAL_PATH_RESET 0x68
+#define MOT_DETECT_CTRL 0x69
+#define USER_CTRL 0x6A
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+#define FIFO_COUNTH 0x72
+#define FIFO_COUNTL 0x73
+#define FIFO_R_W 0x74
+#define WHO_AM_I 0x75
+#define XA_OFFSET_H 0x77
+#define XA_OFFSET_L 0x78
+#define YA_OFFSET_H 0x7A
+#define YA_OFFSET_L 0x7B
+#define ZA_OFFSET_H 0x7D
+#define ZA_OFFSET_L 0x7E
 
 #pragma pack(1)
 typedef struct {
@@ -55,21 +90,111 @@ static void twimCallback(TwimError err);
 bool mpu9250Init(void)
 {
     twimInit(TWIM_SDA_PIN, TWIM_SCL_PIN, twimCallback);
+
+    // reset
+    // mpu9250Reset();
+
+    // write accel config
     AccelConfig accel = {
         .accel_fs_sel = 0x03, // ±2g (00), ±4g (01), ±8g (10), ±16g (11)
-        .az_st_en = 0x01,
-        .ay_st_en = 0x01,
-        .ax_st_en = 0x01,
+        .az_st_en = 0x00,
+        .ay_st_en = 0x00,
+        .ax_st_en = 0x00,
     };
     txBuff[0] = ACCEL_CONFIG;
     txBuff[1] = accel.config;
     busy = twimWrite(0x68, txBuff, 2);
     while(busy);
-    busy = twimRead(0x68, SELF_TEST_X_ACCEL, &accelTest, sizeof(accelTest));
+
+    txBuff[0] = GYRO_CONFIG;
+    txBuff[1] = (1 << 3) | (1 << 4); // GYRO_FS_SEL[1:0] Gyro Full Scale Select: 11 = +2000 dps
+    busy = twimWrite(0x68, txBuff, 2);
+    while(busy);
+
+    // enable bypass mode
+    txBuff[0] = INT_PIN_CFG;
+    txBuff[1] = 1 << 1; // BYPASS_EN
+    busy = twimWrite(0x68, txBuff, 2);
     while(busy);
 
     return true;
 }
+
+bool mpu9250Reset(void)
+{
+    txBuff[0] = PWR_MGMT_1;
+    txBuff[1] = 1 << 7; // H_RESET Reset the internal registers and restores the default settings.
+    if (!busy && twimWrite(0x68, txBuff, 2)) {
+        busy = true;
+        while(busy);
+        return true;
+    }
+    return false;
+}
+
+bool mpu9250DataReady(void)
+{
+    if (!busy && twimRead(0x68, INT_STATUS, rxBuff, 1)) {
+        busy = true;
+        while(busy);
+        return (rxBuff[0] & 0x01) == 1;
+    }
+    return false;
+}
+
+bool mpu9250ReadAccel(AccelData *data)
+{
+    if (!busy && twimRead(0x68, ACCEL_XOUT_H, data, sizeof(AccelData))) {
+        busy = true;
+        while(busy);
+        return true;
+    }
+    return false;
+}
+
+bool mpu9250ReadTemp(TempData *data)
+{
+    // TEMP_degC = ((TEMP_OUT – RoomTemp_Offset)/Temp_Sensitivity) + 21degC
+
+    if (!busy && twimRead(0x68, TEMP_OUT_H, data, sizeof(TempData))) {
+        busy = true;
+        while(busy);
+        return true;
+    }
+    return false;
+}
+
+bool mpu9250ReadGyro(GyroData *data)
+{
+// Low byte of the X-Axis gyroscope output
+// GYRO_XOUT = Gyro_Sensitivity * X_angular_rate
+// Nominal
+// Conditions
+// FS_SEL = 3
+// Gyro_Sensitivity = 16.4 LSB/(º/s)
+
+    if (!busy && twimRead(0x68, GYRO_XOUT_H, data, sizeof(GyroData))) {
+        busy = true;
+        while(busy);
+        return true;
+    }
+    return false;
+}
+
+bool mpu9250ReadAllSensors(AllSensorsData *data)
+{
+    if (!busy && twimRead(0x68, ACCEL_XOUT_H, data, sizeof(AllSensorsData))) {
+        return busy = true;
+    }
+    return false;
+}
+
+// bool mpu9250ReadMagn(MagnData *data)
+// {
+//     busy = twimRead(0x68, INT_STATUS, &data, sizeof(AccelData));
+//     while(busy);
+//     return (rxBuff[0] & 0x01) == 1;
+// }
 
 static void twimCallback(TwimError err)
 {
--- src/mpu9250.h
+++ src/mpu9250.h
@@ -1,4 +1,42 @@
 #include "stdint.h"
 #include "stdbool.h"
 
+#pragma pack(1)
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} AccelData;
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} GyroData;
+
+typedef struct {
+    uint16_t t;
+} TempData;
+
+typedef struct {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+} MagnData;
+
+typedef struct {
+    AccelData accel;
+    TempData temp;
+    GyroData gyro;
+} AllSensorsData;
+#pragma pop
+
 bool mpu9250Init(void);
+bool mpu9250Reset(void);
+bool mpu9250DataReady(void);
+bool mpu9250ReadAccel(AccelData *data);
+bool mpu9250ReadTemp(TempData *data);
+bool mpu9250ReadGyro(GyroData *data);
+bool mpu9250ReadAllSensors(AllSensorsData * data);
+// bool mpu9250ReadMagn(MagnData *data);
+
